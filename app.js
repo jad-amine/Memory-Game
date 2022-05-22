c5b449866064d6cf5fa1a5f55199e78124bed93b
jad amine <jade1992amine@gmail.com>
2022-05-22 10:37:34 +0300

    Added User input and colors pattern

--- app.js
+++ app.js
@@ -25,3 +25,45 @@ function listen_to_wrong_elements(correct_element){
 function stop_listening(){
    list.forEach(e => e.removeEventListener("click", gamer_over));
 }
+
+
+document.addEventListener("click", start_game);
+
+// Start Game
+function start_game(){
+   document.removeEventListener("click", start_game);
+   add_an_element();
+}
+
+// Levels
+function add_an_element(){
+   console.log(`pushed ${level} element`)
+   pattern.push(random_element());
+   highlight(pattern[level]);
+   level += 1;
+   setTimeout(() => h2.innerHTML = `Level ${level}`, 300); 
+   wait_for_user_input();
+}
+
+// Listen for user input 
+function wait_for_user_input(){
+   if(pattern[target]){
+      console.log('waiting for user input')
+      listen_to_wrong_elements(pattern[target]);
+      pattern[target].addEventListener("click", correct)
+   } else {
+      console.log("no more elements => add element")
+      target = 0;
+      add_an_element();
+   }
+}
+
+// Correct answer 
+function correct(){
+   console.log("user clicked the right color")
+   blink(pattern[target]);
+   pattern[target].removeEventListener("click", correct)
+   stop_listening();
+   target+=1;
+   wait_for_user_input();
+}
